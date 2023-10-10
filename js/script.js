75aac78d28f3980a524e403e826be7c8af3d6a11
monoronjonmodok <monoronjonmodok1@gmail.com>
2023-10-10 06:25:28 +0100

    recpte section dynamic

--- js/script.js
+++ js/script.js
@@ -13,22 +13,6 @@ function getProductPrice(productId){
 }
 
 
-// const buy = document.querySelectorAll('.btn-Buy')
-// buy.addEventListener('click', function(){
-//     let append = document.getElementById('selectItem');
-//     const productName = getProductName('productName');
-//     const productPrice =  getProductPrice('price');
-//     const count = append.childElementCount;
-//     const p = document.createElement('p');
-//     p.innerText = count+1 + ' ' + productName + '  $' + productPrice;
-
-//     append.appendChild(p);
-
-// }
-
-// )
-// console.log(buy);
-
 function  buy(button){
     const productName = button.parentNode.parentNode.childNodes[3].innerText;
     const productPrice = parseFloat(button.parentNode.parentNode.childNodes[9].childNodes[1].innerText);
@@ -39,4 +23,47 @@ function  buy(button){
     append.appendChild(p);
     
     // console.log(productPrice);
+    const totalItem =document.getElementById('totalItem');
+    totalItem.innerText = count+1;
+    let totalPrice = parseFloat(document.getElementById('totalPrice').innerText);
+    totalPrice = totalPrice+productPrice;
+    document.getElementById('totalPrice').innerText=totalPrice;
+
+    
+
+ if(totalPrice>200){
+    let discount = document.getElementById('discountPrice');
+    let payamt = document.getElementById('payamount');
+    const discountPrice =(totalPrice*20)/100;
+        discount.innerText = discountPrice;
+        const paytotal = totalPrice - parseFloat(discount.innerText);
+        payamt.innerText= paytotal;
+       
+ }
+
+
+
 }
+
+/* discount price part */
+ const apply = document.getElementById('apply-btn');
+
+apply.addEventListener('click', function(){
+    let cuponvalue = document.getElementById('cuponn').value;
+    const cupponCood = 'less20';
+    let totalPrice = parseFloat(document.getElementById('totalPrice').innerText);
+    let discount = document.getElementById('discountPrice');
+    let payamt = document.getElementById('payamount');
+
+    if(cuponvalue === cupponCood && totalPrice<200 ){
+        const discountPrice =(totalPrice*20)/100;
+        discount.innerText = discountPrice;
+        const paytotal = totalPrice - parseFloat(discount.innerText);
+        payamt.innerText= paytotal;
+        alert('congratulation you are 10% discount')
+        }
+    else{
+     alart('your cuppon not valid')
+    }
+})
+ 
